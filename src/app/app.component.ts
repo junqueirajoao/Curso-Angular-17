dd75e8000b3f3c6ade5bb2697d6cf4fe5a7671d4
João <junqueira@hotmail.com>
2024-10-04 21:41:55 -0300

    Componente 01 e 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from "./componente01/componente01.component";
+import { Componente02Component } from "./componente02/componente02.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componente01Component, Componente02Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
