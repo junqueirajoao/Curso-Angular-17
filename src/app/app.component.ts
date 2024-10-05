de621413a8decb348aa565976bb934676c2df520
João <junqueira@hotmail.com>
2024-10-04 22:19:35 -0300

    Aula 10 - Diretiva ngSwitch - Componente06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { Componente02Component } from "./componente02/componente02.component";
 import { Componente03Component } from "./componente03/componente03.component";
 import { Componente04Component } from "./componente04/componente04.component";
 import { Componente05Component } from "./componente05/componente05.component";
+import { Componente06Component } from "./componente06/componente06.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component],
+  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
