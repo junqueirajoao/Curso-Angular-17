df0e88849fcef2c03bbb1d535e333de944179c39
João <junqueira@hotmail.com>
2024-10-04 22:27:13 -0300

    Aula 11 - Componente 07 - Uso do ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Componente03Component } from "./componente03/componente03.component";
 import { Componente04Component } from "./componente04/componente04.component";
 import { Componente05Component } from "./componente05/componente05.component";
 import { Componente06Component } from "./componente06/componente06.component";
+import { Componente07Component } from "./componente07/componente07.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component],
+  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
